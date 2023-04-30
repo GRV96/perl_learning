04f2032c6320afb6369ac75a789debf83b9dd147
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-04-30 16:33:29 -0400

    A script shows how to copy arrays and hashes.

---
+++ copy_test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+@an_array = (0, 1, 2, 3, 4);
+@an_arry_copy = @an_array;
+
+%a_hash = ("zero", 0, "one", 1, "two", 2);
+%a_hash_copy = %a_hash;
+
+#push(@an_array_copy, 5);
+$an_arry_copy[1] = 53;
+$a_hash_copy{"two"} = 7;
+
+print "\@an_array: @an_array\n";
+print "\@an_array_copy: @an_array_copy\n";
+
+print "\n\$a_hash{\"two\"}: $a_hash{'two'}\n";
+print "\$a_hash_copy{\"two\"}: $a_hash_copy{'two'}\n";
