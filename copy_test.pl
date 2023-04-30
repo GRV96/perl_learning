1745ce6ce6db0f11a229c4395b897838216f1d99
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-04-30 16:47:27 -0400

    Typo in a variable name

--- copy_test.pl
+++ copy_test.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl
 
 @an_array = (0, 1, 2, 3, 4);
-@an_arry_copy = @an_array;
+@an_array_copy = @an_array;
 
 %a_hash = ("zero", 0, "one", 1, "two", 2);
 %a_hash_copy = %a_hash;
 
-#push(@an_array_copy, 5);
-$an_arry_copy[1] = 53;
+push(@an_array_copy, 5);
+$an_array_copy[1] = 53;
 $a_hash_copy{"two"} = 7;
 
 print "\@an_array: @an_array\n";
