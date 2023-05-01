8853c05ee5c165554a8ed2ca24aac7cdf50d2253
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-04-30 22:52:09 -0400

    Subroutines in run_subs.pl

--- run_subs.pl
+++ run_subs.pl
@@ -2,11 +2,29 @@
 
 require("./math.pl");
 
-@numbers = (12, 30, 53, 18, 76, 92, 45);
-print "Average of @numbers: " . average(@numbers) . "\n";
+sub run_average {
+	print "Average of @_: " . average(@_) . "\n";
+}
 
-print "17! = " . factorial(17) . "\n";
+sub run_factorial {
+	my $number = $_[0];
+	print "$number! = " . factorial($number) . "\n";
+}
 
-print "gcd(-56, 72): " . gcd(-56, 72) . "\n";
+sub run_gcd {
+	my $a = $_[0];
+	my $b = $_[1];
+	print "gcd($a, $b) = " . gcd($a, $b) . "\n";
+}
 
-print "lcm(2, 3, 4, 5, 7): " . lcm(2, 3, 4, 5, 7) . "\n";
+sub run_lcm {
+	print "lcm(@_) = " . lcm(@_) . "\n";
+}
+
+run_average(12, 30, 53, 18, 76, 92, 45);
+
+run_factorial(17);
+
+run_gcd(-56, 72);
+
+run_lcm(2, 3, 4, 5, 7);
