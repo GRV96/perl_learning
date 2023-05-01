eb771dfb28ffd63ddd97639db5fb8305e69ac903
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-05-01 17:00:22 -0400

    Several tests

--- run_subs.pl
+++ run_subs.pl
@@ -22,9 +22,25 @@ sub run_lcm {
 }
 
 run_average(12, 30, 53, 18, 76, 92, 45);
+print "\n";
 
 run_factorial(17);
+run_factorial(18);
+run_factorial(19);
+run_factorial(20);
+run_factorial(21);
+print "\n";
 
+run_gcd(56, 72);
 run_gcd(-56, 72);
+run_gcd(960, 756);
+run_gcd(780, -960);
+run_gcd(51, 21);
+run_gcd(24, 85);
+#run_gcd(, );
+#run_gcd(, );
+#run_gcd(, );
+print "\n";
 
 run_lcm(2, 3, 4, 5, 7);
+print "\n";
